105101086f254f64964ab74b8ebb4d87a2e81f2e
abangayoma <abangayoma@gmail.com>
2024-01-22 10:43:23 +0000

    created 1-multi-language

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const statementOne; 
+const statementTwo;
+const statementThree;
+statementOne = 'C is fun';
+statementTwo = 'Python is cool';
+statementThree = 'JavaScript is amazing';
+
+console.log(statementOne, statementTwo, statementThree);
