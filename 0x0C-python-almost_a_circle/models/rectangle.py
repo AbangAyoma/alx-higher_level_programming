0412867336551e6ddb77bb2d631b16d93ebae138
abangayoma <abangayoma@gmail.com>
2024-04-27 00:30:07 +0100

    created a rectangle.py file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,4 @@
-from base import Base
+from models.base import Base
 """A class called Rectangle that inherits from the Base class"""
 class Rectangle(Base):
 	"""the class takes private attributes"""
@@ -12,45 +12,39 @@ class Rectangle(Base):
 		self.height = height
 		self.__x = x
 		self.__y = y
-		super().__init__
+		super().__init__(id)
 	'''Setting getters and setters for all attribute of the 
     rectanlge class'''
 	@property
 	def width(self):
-        '''getter for the width attribute'''
 		return self.__width
 	
 	@width.setter
     def width(self, value):
-        '''setter for the width attribute'''
-        self.__width = width
-		
+        self.__width = value
+    
     @property
 	def height(self):
-        """getter for the width attribute"""
 		return self.__width
 	
 	@width.setter
     def height(self, value):
-        '''setter for the height attribute'''
-        self.__width = width
+        self.__width = value
 	
     @property
     def x(self):
-        """getter for the x attribute"""
         return self.__x
     
     @x.seter
     def x(self, value):
         '''setter for the x attribute'''
-        self.__x = x
+        self.__x = value
     
     @property
     def y(self):
-        """getter for the y attribute"""
         return self.__y
     
     @y.seter
     def y(self, value):
-        '''setter for the y attribute'''
-        self.__y = y
+        self.__y = value
+    
