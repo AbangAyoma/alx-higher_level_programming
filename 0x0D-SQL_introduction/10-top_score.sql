c3dad266ba50492b7f66a963e57850bd0fef8425
Abang Laz <113807236+AbangAyoma@users.noreply.github.com>
2024-05-24 00:44:04 +0100

    Update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -2,5 +2,5 @@
 -- Results should display both the score and the name (in this order)
 -- Records should be ordered by score (top first)
 -- The database name will be passed as an argument of the mysql commanD
-SELECT score FROM second_table ORDER BY DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
 exit
