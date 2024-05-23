cdf3df109eca9ca9c7cad82d30ff0882e08a27d4
AbangAyoma <abangayoma@gmail.com>
2024-05-24 00:15:31 +0100

    created m

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,5 @@
 -- script to insert 
 
-ALTER TABLE first_table SET id INT (8)= 89;
-ALTER TABLE first_table SET name VARCHAR(100) = 'Best School';
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+exit
  
