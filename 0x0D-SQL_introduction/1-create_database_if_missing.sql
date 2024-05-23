64e54503401eb6c2adc722d092aa8574ed616b9f
AbangAyoma <abangayoma@gmail.com>
2024-05-23 16:00:23 +0100

    created the files 1-create_database and 2-delete_database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- This script create a database 
+-- only if the database doesn't exist!
+
+CREATE DATABASE  IF NOT EXISTS hbtn_0c_0;
+exit
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- This script delete a database 
+-- only if the database doesn't exist!
+
+DELETE DATABASE  IF NOT EXISTS hbtn_0c_0;
+exit
