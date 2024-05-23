455b36e97667154302e7108e864f1a451c36a13b
AbangAyoma <abangayoma@gmail.com>
2024-05-23 15:43:39 +0100

    uptadet

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,5 +1,4 @@
--- This script uses the sql SHOW command
--- to show all databases in the system!
+-- This script uses the sql SHOW command to show all databases in the system!
 
 SHOW DATABASE;
 exit
