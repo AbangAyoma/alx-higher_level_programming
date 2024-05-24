bf6485d88d79d474ba46509f2b4e92777b8b5c53
AbangAyoma <abangayoma@gmail.com>
2024-05-24 13:59:49 +0100

    created 16- n o_links.sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,4 @@
+-- A script that list all records of the table second_table of the database hbtn_0c_0
+-- Don't list rows without a name value, records should be in descending!
+SELECT score, name FROM second_table  ORDER BY score DESC;
+exit
