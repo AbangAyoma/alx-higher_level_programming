40059b2303dd1685703d551789c5969bc5762c9e
AbangAyoma <abangayoma@gmail.com>
2024-05-24 13:48:24 +0100

    updated 13-chnge_class

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -2,5 +2,5 @@
 -- in the table second_table of the database hbtn_0c_0
 -- the database will be passes as an argument of the sql command!
 
-DELETE FROM second_table WHERE score>=80;
+DELETE FROM second_table WHERE score<=5;
 exit
